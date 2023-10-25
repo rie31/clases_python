55c029584df5a5849c2e9081bbfe887d9857944a
rie31 <rocioriera31@gmail.com>
2023-10-25 08:12:40 -0300

    Clase 18-10

---
+++ clase_18_10.py
@@ -0,0 +1,48 @@
+#Controladores de flujo
+"""a=5
+if a==5:
+    print("a vale 5")
+if a==2:
+    print("A vale 2")
+
+"""
+#IF ELIF ELSE
+
+"""nota= float(input("Introduce una nota: "))
+if nota>=9:
+    print("La rompiste")
+elif nota>=7:
+    print("Buen trabajo")
+elif nota>=6:
+    print("Esta bien..")
+elif nota>=5:
+    print("Suficiente")
+else:
+    print("Desaprobaste")
+"""
+#ITERACIONES
+#WHILE #BREAK
+
+"""c=0
+while c<=5:
+    c+=1
+    print("C vale ", c)
+"""
+"""c=0
+while c<=5:
+    c+=1
+    if(c==4):
+        print("Rompemos el bucle cuando c vale: ", c)
+        break
+    print("c vale: ", c)
+else:
+    print("Ha finalizado la iteracion y el valor de c es: ",c)
+"""
+c=0
+while c<=5:
+    c+=1
+    if c==3 or c==4:
+        continue
+    print("c vale: ",c)
+else:
+    print("Iteracion terminada c vale ",c)
