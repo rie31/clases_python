618fc42722b0a9cc56d5b9e8d7aa01ab2f1530f3
rie31 <rocioriera31@gmail.com>
2023-10-31 23:14:22 -0300

    Clase 18_1 parte 1

--- clase_18_10.py
+++ clase_18_10.py
@@ -1,14 +1,13 @@
 #Controladores de flujo
-"""a=5
+a=5
 if a==5:
     print("a vale 5")
 if a==2:
     print("A vale 2")
-
-"""
+print("--------------------------------------------------------------------------------------")
 #IF ELIF ELSE
 
-"""nota= float(input("Introduce una nota: "))
+nota= float(input("Introduce una nota: "))
 if nota>=9:
     print("La rompiste")
 elif nota>=7:
@@ -19,33 +18,30 @@ elif nota>=5:
     print("Suficiente")
 else:
     print("Desaprobaste")
-"""
+print("--------------------------------------------------------------------------------------")
 #ITERACIONES
 #WHILE #BREAK
-
-"""c=0
-while c<=5:
-    c+=1
-    print("C vale ", c)
-"""
-"""c=0
-while c<=5:
-    c+=1
-    if(c==4):
-        print("Rompemos el bucle cuando c vale: ", c)
+b=0
+while b<=5:
+    b+=1
+    print("b vale ", b)
+print("--------------------------------------------------------------------------------------")
+d=0
+while d<=5:
+    d+=1
+    if(d==4):
+        print("Rompemos el bucle cuando d vale: ", d)
         break
-    print("c vale: ", c)
+    print("d vale: ", d)
 else:
-    print("Ha finalizado la iteracion y el valor de c es: ",c)
-"""
-"""c=0
-while c<=5:
-    c+=1
-    if c==3 or c==4:
+    print("Ha finalizado la iteracion y el valor de d es: ",d)
+print("--------------------------------------------------------------------------------------")
+e=0
+while e<=5:
+    e+=1
+    if e==3 or e==4:
         continue
-    print("c vale: ",c)
+    print("e vale: ",e)
 else:
-    print("Iteracion terminada c vale ",c)
-    """
-
-
+    print("Iteracion terminada e vale ",e)
+    
